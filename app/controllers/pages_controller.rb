2eaca9b42e804f72d181ab92c38b754fbdb892fd
Tavish <tapegram@gmail.com>
2016-08-29 02:09:39 +0000

    Added username prop to user and tidied up logging in / logging out. Still need to redirect to home on login.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,13 @@ class PagesController < ApplicationController
   end
 
   def home
+    # grab the username from the URL as :id
+    if (User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      # redirects to 404 (root for now)
+      redirect_to root_path, :notice => "User not found!"
+    end
   end
 
   def profile
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#index"
 
-  get 'home' => "pages#home"
+  get 'user/:id' => "pages#home"
 
   get 'profile' => "pages#profile"
 
